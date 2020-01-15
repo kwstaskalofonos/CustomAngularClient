ffcea5fc55e27d6f7b730e47706f14de093d4cb3
Kwstas Kalofonos <konkalof1@gmail.com>
2020-01-15 08:47:36 +0200

    Added login component and Form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import {FormsModule} from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { TestComponentComponent } from './test-component/test-component.component';
+import { NavComponent } from './nav/nav.component';
+import { AdminNavComponent } from './admin-nav/admin-nav.component';
+import { AdminPanelComponent } from './admin-panel/admin-panel.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
    declarations: [
       AppComponent,
-      TestComponentComponent
+      TestComponentComponent,
+      NavComponent,
+      AdminNavComponent,
+      AdminPanelComponent,
+      LoginComponent
    ],
    imports: [
       BrowserModule,
-      HttpClientModule
+      HttpClientModule,
+      FormsModule
    ],
    providers: [],
    bootstrap: [
